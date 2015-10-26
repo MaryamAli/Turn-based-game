6333f5bfc0fbfba50205a0c5149c2a318b14f6ef
Maryam Ali <mali62541@gmail.com>
2015-10-26 02:18:59 -0400

    positioning gif

--- js/main.js
+++ js/main.js
@@ -1,17 +1,59 @@
 import $ from 'jquery';
 import _ from 'underscore';
-import moment from 'moment';
 
-console.log('Hello, World');
-
-import Goodguy from './GoodGuy';
-import Badguy from './BadGuy';
-
-
+import GoodGuy from './GoodGuy';
+import BadGuy from './BadGuy';
 
 
 // Good Guy Instance
 let Sherlock = new GoodGuy();
 
 // Bad Guy Instance
-let Moriarty= new BadGuy();
+let Moriarty = new BadGuy();
+
+
+// DOM Nodes Selected
+let ggIntelligence = $('.ggIntelligence');
+let bgIntelligence = $('.bgIntelligence');
+
+let ggInsult = $('.ggInsult');
+
+// Show current (default) intelligence
+ggIntelligence.text(Sherlock.intelligence);
+bgIntelligence.text(Moriarty.intelligence);
+
+
+// Setting up ON Events
+ggInsult.on('click', function () {
+
+  // Generate random insult points
+  // insult
+  let num = _.random(0, 25);
+  Moriarty.insult(num);
+
+  if (Moriarty.intelligence <= 0) {
+    bgIntelligence.text('Mewling Quim!');
+    alert('Sherlock Remains the Most Perspicacious')
+  } else {
+    bgIntelligence.text(Moriarty.intelligence);
+    alert('Moriarty Sharpens His Wit >:|');
+    Sherlock.insult(10);
+    ggIntelligence.css('color', 'red');
+    ggIntelligence.text(Sherlock.intelligence);
+    setTimeout( function () {
+      ggIntelligence.css('color', 'black');
+    }, 1000);
+  }
+
+  console.log(moriarty);
+})
+
+
+
+
+
+// Good Guy Instance
+// let Sherlock = new GoodGuy();
+
+// Bad Guy Instance
+// let Moriarty= new BadGuy();
